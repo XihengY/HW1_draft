d1f6599a5b1cd3ef097fac57f9eb008a4a637037
XihengY <92489126+XihengY@users.noreply.github.com>
2021-10-17 16:27:38 -0700

    Delete test_fileio.sh

--- test_fileio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for((i=0;i<5;i++))
-do
-  sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw prepare &&
-  sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw run &&
-  sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw cleanup
-done
